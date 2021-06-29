8979edeab929ff4b21ee143ecb2566bc16b983e8
lefkal <kalonarisl@gmail.com>
2021-06-29 11:43:18 +0300

    storytelling-nuts

---
+++ CIRCTER/README.txt
@@ -0,0 +1,46 @@
+# ESPON archive for the project 'CIRCTER - Circular Economy and Territorial Consequences'
+
+This readme file describes the content of this project archive.
+
+
+## Information
+
+- Name: CIRCTER - Circular Economy and Territorial Consequences
+- Url: https://www.espon.eu/circular-economy
+- Id: 26
+
+
+### Abstract
+
+The current economy operates mainly on a ‘take-make-dispose’ model – a linear model where every product is bound to reach its ‘end of life’. This leads to a growing pressure of production and consumption on the world’s resources and environment. A circular economy model strives towards using and re-using our resources in a more resource efficient through preserving and enhancing natural capital, optimising natural resource yields by circulating products, components and materials, as well as fostering effective waste management and minimizing detrimental practices.
+
+This dataset provides evidence on the territorial dimension of the transition towards a Circular Economy in Europe by informing on patterns and flows of materials, including resources and waste, as well as on the sectoral distribution of circular economy activities. All datasets are delivered at NUTS-2 level.
+
+
+# Files
+
+- README.txt: This file
+
+
+## Main Data
+
+- main-data/ind_1055_dmc_csv_47MSlNR.zip: Domestic Material Consumption (DMC)
+- main-data/ind_1056_de_csv_VwKIoeh.zip: Domestic Extraction (DE)
+- main-data/ind_1059_was_csv_BdPci0d.zip: Total waste generation, excluding major mineral wastes (WAS)
+- main-data/ind_1061_cemp_tur_csv.zip: Circular economy material providers (turnover) (CEMP_TUR)
+- main-data/ind_1064_cbm_emp_csv_9V9Kc03.zip: Circular economy business models (employment) (CBM_EMP)
+- main-data/ind_1060_cemp_emp_csv.zip: Circular economy material providers (employment) (CEMP_EMP)
+- main-data/ind_1065_cbm_tur_csv_0lhLd5c.zip: Circular economy business models (turnover) (CBM_TUR)
+- main-data/ind_1063_cetp_tur_csv_0hbuLwX.zip: Circular economy technology Providers (turnover) (CETP_TUR)
+- main-data/ind_1062_cetp_emp_csv_m8Cdppq.zip: Circular economy technology Providers (employment) (CETP_EMP)
+
+
+## Other Data
+
+- Municipal waste indicators, other data
+  - other-data/DB2020_Municipal_waste_OtherData_n9ZRi5y.xls: Municipal waste indicators, other data
+- Waste generation indicators
+- Sectoral indicators of the Circular Economy, other data
+  - other-data/DB2020_Sectoral_indicators_OtherData_PROGNOS_17JUN19.xls: Sectoral indicators of the Circular Economy, other data
+- Material consumption indicators, other data.
+
---
+++ config.js
@@ -0,0 +1,157 @@
+var config = {
+    style: 'mapbox://styles/elkalonaris/ckqgn4fhs0su617qgffpwotrj',
+    accessToken: 'pk.eyJ1IjoiZWxrYWxvbmFyaXMiLCJhIjoiY2twbXExOHcwMDg3ZzJvbnc0dXhyOWszOSJ9.AetSK7t4L_aaFVsCuNQmHg',
+    showMarkers: false,
+    theme: 'light',
+    title: '',
+    subtitle: '',
+    byline: '',
+    footer: '',
+    chapters: [
+        {
+            id: 'phl',
+            alignment: 'right',
+            title: 'Change in annual mean number of days with heavy rainfall',
+            image: '',
+            description: 'Getting around Philadelphia on two wheels is fast, fun, and cheap. As a typical East Coast large city, the urban core is dense, so there is a lot within reach of a 15 minute ride... even mountain bike trails. Paired with the public transit infrastructure, cycling can be more efficient and much less expensive than driving (and parking) a car.',
+            location: {
+                center: [12, 50],
+                zoom: 2.99,
+                pitch: 0.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {
+                    layer: 'terrain',
+                    opacity: .95
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'terrain',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'bike-lanes',
+            alignment: 'right',
+            title: 'Bike Lanes',
+            image: '',
+            description: 'In territorial units: 97.00 / In GDP: / In area: 96.00 / In population: 79.00',
+            location: {
+                center: [12, 50],
+                zoom: 11.62,
+                pitch: 55.50,
+                bearing: -7.20
+            },
+            onChapterEnter: [
+                {
+                    layer: 'terrain',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: []
+        },
+        {
+            id: 'indego',
+            alignment: 'right',
+            title: 'Indego Bike Share',
+            image: '',
+            description: 'Indego has been operating in Philadelphia since 20XX. The system initally was focused on Center City, but has expanded service to neighboring areas to support equitable mobility options to the city\'s residents.',
+            location: {
+                center: [-75.16468, 39.94503],
+                zoom: 13.15,
+                pitch: 60.00,
+                bearing: -16.80
+            },
+            onChapterEnter: [
+                {
+                    layer: 'indego-stations',
+                    opacity: 0.8
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'indego-stations',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'belmont',
+            alignment: 'right',
+            title: 'Belmont Plateau Trails',
+            image: '',
+            description: 'A short ride along the Schuylkill River Trail from the Art Museum, Belmont is a twisty, log-ridden rollercoaster of a trail network. It is easy to get turned around, the underbrush is at times impenetrable, and short steep sections come out of nowhere. In other words, it\'s really fun',
+            location: {
+                center: [-75.20325, 39.99574],
+                zoom: 14.99,
+                pitch: 44.00,
+                bearing: -40.00
+            },
+            onChapterEnter: [
+                {
+                    layer: 'belmont',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'belmont',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'wiss',
+            alignment: 'right',
+            title: 'Wissahickon Park Trails',
+            image: '',
+            description: 'This steep, rocky gorge can be surprisingly technical. Follow the orange and yellow trails to repeatedly climb and descend through the schist hillsides (careful of the cliffs), or stick to the gravel Forbidden Drive for a relaxing ride along the creek. You\'ll forget you\'re in a city.',
+            location: {
+                center: [-75.21223, 40.05028],
+                zoom: 13.08,
+                pitch: 47.50,
+                bearing: 32.80
+            },
+            onChapterEnter: [
+                {
+                    layer: 'wissahickon',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'wissahickon',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'pennypack',
+            alignment: 'right',
+            title: 'Pennypack Park Trails',
+            image: '',
+            description: 'Pennypack is a great introduction trail system. Not too steep and not too technical, the beautiful wooded park also provides a great escape from urban life. The south side trails are originally bridle trails, so be nice to equestrians and dismount when you approach them.',
+            location: {
+                center: [-75.05685, 40.06839],
+                zoom: 13.73,
+                pitch: 43.50,
+                bearing: 96.80
+            },
+            onChapterEnter: [
+                {
+                    layer: 'pennypack',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'pennypack',
+                    opacity: 0
+                }
+            ]
+        }
+    ]
+};
